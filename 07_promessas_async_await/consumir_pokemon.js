002e2bc28abf950cb1c11159653fb9cb55861820
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-06-05 17:12:35 -0300

    ft: API pokemon example

--- 07_promessas_async_await/consumir_pokemon.js
+++ 07_promessas_async_await/consumir_pokemon.js
@@ -1,11 +1,14 @@
 function consumirAPI() {
   fetch("https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/sp")
     .then((response) => {
+    // Método .json() transforma os textos brutos em objetos JavaScripts para lermos
       return response.json()
     })
     .then((data) => {
+        // Abrindo o conteúdo e usando os dados
         console.log(data.state)
     })
+    // Emite error
     .catch((error) => console.log(error));
 }
 
